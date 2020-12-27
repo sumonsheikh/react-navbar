89d53123b5839049a38a02bfa04bb6a36bbcdfbd
sumonsheikh <sm.sumon117@gmail.com>
2020-12-27 22:26:17 +0600

    create list item and social link icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,10 +7,49 @@ const  Navbar = () => {
             <div className="nav-center">
                 <div className="nav-header">
                     <h2><a href="#">Logo</a></h2>
-                    <button>
+                    <button className="nav-toggle">
                         <FaBars/>
                     </button>
                 </div>
+                <div className="link-container show-container">
+                    <ul className="links">
+                        <li>
+                            <a href="#">home</a>
+                        </li>
+                        <li>
+                            <a href="#">about</a>
+                        </li>
+                        <li>
+                            <a href="#">contact</a>
+                        </li>
+                        <li>
+                            <a href="#">product</a>
+                        </li>
+                       
+                    </ul>
+                </div>
+                <ul className="social-icons">
+                    <li>
+                        <a href="https://www.twitter.com">
+                            <FaTwitter/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com">
+                            <FaTwitter/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com">
+                            <FaTwitter/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com">
+                            <FaTwitter/>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
